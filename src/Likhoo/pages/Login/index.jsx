2ace87bbc68d34243bcf9ee60524de83bfa5d1af
chandanbauri <chandanbauri441@gmail.com>
2021-01-31 12:57:58 +0530

    login and signup screen modified

--- src/Likhoo/pages/Login/index.jsx
+++ src/Likhoo/pages/Login/index.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const initialState = {
+        username: "",
+        password: "",
+    };
+    const [app, setApp] = React.useState(initialState);
+    const handleTextInput = (e) => {
+        setApp({ ...app, [e.target.name]: e.target.value });
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(app);
+    }
     return (
-        <div>
-            Login Screen
-        </div>
+        <main className="lko-main">
+            <section className="lko-login-section">
+                <span id="signup-text">Login</span>
+                {/* <!-- <form action="/login" method="get"> --> */}
+                <form action="index.html" method="get">
+                    {/* <!-- <label for="username">Username</label> --> */}
+                    <input type="text" id="username" name="username" placeholder="Email Address or Username" onChange={handleTextInput} />
+                    {/* <!-- <label for="password">Password</label> --> */}
+                    <input type="password" id="password" name="password" placeholder="Password" onChange={handleTextInput} />
+                    <input type="submit" value="Login" onClick={handleSubmit} />
+                </form>
+                {/* <!-- <hr> --> */}
+                <span style={{ alignSelf: "center", margin: "10px" }}>or</span>
+                {/* <!-- <hr> --> */}
+                <Link to="/signup" id="lko-new-acc">Create New Account</Link>
+            </section>
+        </main>
     )
 }
 
